d614a5be8d8ae2c82bd4a95a151f1d4cf7035875
Felipee ;3 <97256147+akaFelipee@users.noreply.github.com>
2024-11-18 18:12:54 -0300

    Create lista.c
    Arquivo criado e observações/rascunhos adicionados

---
+++ Projeto1a/lista.c
@@ -0,0 +1,38 @@
+// Arquivo para as funções
+
+/* OBSERVAÇÕES:
+1. Coloquei uns rascunhos, pode alterar o que quiser
+2. Os tópicos são repetidos, por isso tem "3.3 & 5.3", no final a gente pode deixar só "5.3" por exemplo
+3. Os tópicos 3.7 e 6 são iguais, o problema é que o 3.7 é meu e o 6 é seu, tem que ver quem vai fazer o que
+4. Coloquei os parênteses no final pra gente se guiar, mas pode apagar depois se quiser
+4. Lembrar de apagar essas observações ao fim do projeto kk
+*/ 
+
+#include "lista.h"
+
+// 3.1 & 3.2 & 5.1 Inserção de Produto (Juliana)
+
+
+// 3.3 & 5.2 Remoção de Produto (Juliana)
+
+
+// 3.4 & 5.3 Busca de Produto (Juliana)
+
+
+// 3.5 & 5.4 Atualização de Produto (Felipe)
+
+
+// 3.6 Listagem de Produtos (Felipe)
+
+
+// 3.7.1 & 6.1 Gravação de Dados (Ambos?)
+
+
+// 3.7.2 & 6.2 Carregamento de Dados (Ambos?)
+
+
+// 7 Redimensionamento do Vetor (Felipe)
+
+
+// (Adicionar mais conforme a necessidade)
+
