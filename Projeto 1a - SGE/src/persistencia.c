3ccce2a2bf8ae7b5eecbf26e18ac663c20182bda
Felipee ;3 <97256147+akaFelipee@users.noreply.github.com>
2024-11-27 22:31:06 -0300

    Implementado persistencia.c

--- Projeto 1a - SGE/src/persistencia.c
+++ Projeto 1a - SGE/src/persistencia.c
@@ -1,4 +1,36 @@
-// Arquivo para as implementações de funções ref. à manipulação de arquivos
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "persistencia.h"
+
+void carregarEstoque(Estoque *estoque, const char *caminhoArquivo) {
+    FILE *arquivo = fopen(caminhoArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Nao foi possivel abrir o arquivo %s .\n", caminhoArquivo);
+        return;
+    }
+
+    while (!feof(arquivo)) {
+        Produto p;
+        if (fscanf(arquivo, "%d,%49[^,],%d,%f\n", &p.codigo, p.nome, &p.quantidade, &p.preco) == 4) {
+            inserirProduto(estoque, p);
+        }
+    }
+
+    fclose(arquivo);
+}
+
+void salvarEstoque(Estoque *estoque, const char *caminhoArquivo) {
+    FILE *arquivo = fopen(caminhoArquivo, "w");
+    if (arquivo == NULL) {
+        printf("Nao foi possivel abrir o arquivo %s .\n", caminhoArquivo);
+        return;
+    }
+
+    for (int i = 0; i < estoque->tamanho; i++) {
+        Produto p = estoque->produtos[i];
+        fprintf(arquivo, "%d,%s,%d,%.2f\n", p.codigo, p.nome, p.quantidade, p.preco);
+    }
+
+    fclose(arquivo);
+}
